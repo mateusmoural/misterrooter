a0ff86fce0db64a573767c22b2cfc054da5d3c37
mateusmoural <63815595+mateusmoural@users.noreply.github.com>
2020-09-13 16:27:09 -0300

    incluir script

---
+++ v0.1/bootstrap-4.5.2/js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+     
+    var menuBtn = document.querySelector('.menu-btn');
+var nav = document.querySelector('nav');
+var lineOne = document.querySelector('header .menu-btn .line--1');
+var lineTwo = document.querySelector('header .menu-btn .line--2');
+var lineThree = document.querySelector('header .menu-btn .line--3');
+var link = document.querySelector('nav .nav-links');
+menuBtn.addEventListener('click', () => {
+nav.classList.toggle('nav-open');
+lineOne.classList.toggle('line-cross');
+lineTwo.classList.toggle('line-fade-out');
+lineThree.classList.toggle('line-cross');
+link.classList.toggle('fade-in');
+})
+
+});
+
+
+
+$(window).scroll(function() {    
+var scroll = $(window).scrollTop();
+
+if (scroll >= 50) {
+   $("body").addClass("animation");
+} else {
+   $("body").removeClass("animation");
+}
+});
